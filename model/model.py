300e1b12fd93c1efe523f240183c508b5c19694a
SpadaccinoPungente <s297508@studenti.polito.it>
2026-04-27 16:22:10 +0200

    Final commit.

--- model/model.py
+++ model/model.py
@@ -1,3 +1,5 @@
+import copy
+
 from database.meteo_dao import MeteoDao
 
 
@@ -47,9 +49,70 @@ class Model:
     """
 
     def calcola_sequenza(self, mese):
-        # Condizione terminale
-        # if:
-        # Ricorsione
-        # else:
-        pass
+        self._best_sol = []
+        self._best_costo = float('inf')
+        self.ricorsione([], self.filtra_situazioni_15gg(self._situazioni, mese), 0)
+        # situazioni già ordinate
+        return self._best_sol, self._best_costo
 
+    # @lru_cache
+    def ricorsione(self, parziale, situazioni, costo):
+
+        # BRANCH AND BOUND: Taglio i rami morti
+        if costo >= self._best_costo: return
+
+        if len(parziale)==15:
+            if costo < self._best_costo:
+                self._best_sol = copy.deepcopy(parziale)
+                self._best_costo = costo
+        else:
+            for situazione in situazioni[3*len(parziale):3*len(parziale)+3]:
+                # Funziona ma in generale pericoloso. Il modo corretto sarebbe ciclare sulle città filtrando per
+                # il giorno esatto: giorno_corrente = len(parziale) + 1, e cercare nella lista situazioni
+                # solo gli elementi con Data.day == giorno_corrente.
+
+                parziale.append(situazione)
+
+                # La città inserita non deve superare i 6 giorni totali.
+                if self.check_max_6gg(parziale):
+
+                    # Sto cambiando città?
+                    if len(parziale) > 1 and parziale[-2].Localita != situazione.Localita:
+                        # Sì -> controllo dei 3 giorni minimi.
+                        if self.check_min_3gg(parziale):
+                            nuovo_costo = costo + 100 + situazione.Umidita
+                            self.ricorsione(parziale, situazioni, nuovo_costo)
+                    else:
+                        # No (o è il primo giorno).
+                        nuovo_costo = costo + situazione.Umidita
+                        self.ricorsione(parziale, situazioni, nuovo_costo)
+
+                parziale.pop()
+
+    @staticmethod
+    def filtra_situazioni_15gg(situazioni, mese):
+        situazioni_15gg_mese = []
+        for situazione in situazioni:
+            if situazione.Data.month == mese and situazione.Data.day <= 15:
+                situazioni_15gg_mese.append(situazione)
+        return situazioni_15gg_mese
+
+    @staticmethod
+    def check_max_6gg(parziale):
+        conteggio = sum(1 for s in parziale if s.Localita == parziale[-1].Localita)
+        return conteggio <= 6
+
+    @staticmethod
+    def check_min_3gg(parziale):
+        if len(parziale) <= 3: return False
+        return parziale[-3].Localita == parziale[-2].Localita and parziale[-4].Localita == parziale[-2].Localita
+
+    # Corretto ma inutilmente complicato:
+    # @staticmethod
+    # def check_min_3gg(parziale):
+    #     cnt_cons_gg = 0
+    #     for i in range(len(parziale)-2, -1, -1):
+    #         if parziale[i].Localita == parziale[-2].Localita:
+    #             cnt_cons_gg += 1
+    #         else: return cnt_cons_gg >= 3
+    #     return cnt_cons_gg >= 3 # All'uscita, deve verificare di aver raggiunto i 3 giorni
